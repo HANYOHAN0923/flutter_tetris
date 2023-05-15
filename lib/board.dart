6024ae352d700a641187cfac44cfa4cbe8275ae5
한요한 <johnhan@han-yohan-ui-Macmini.local>
2023-05-15 22:37:34 +0900

    collision test

--- lib/board.dart
+++ lib/board.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:tetris_game/piece.dart';
 import 'package:tetris_game/pixel.dart';
+import 'package:tetris_game/values.dart';
 
 class GameBoard extends StatefulWidget {
   const GameBoard({super.key});
@@ -9,9 +13,48 @@ class GameBoard extends StatefulWidget {
 }
 
 class _GameBoardState extends State<GameBoard> {
-  // grid dimensions
-  int rowLength = 10;
-  int colLength = 15;
+  // current tetris piece
+  Piece currentPiece = Piece(type: Tetromino.T);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    startGame();
+  }
+
+  void startGame() {
+    currentPiece.initializePiece();
+
+    // frame refresh rate
+    Duration frameRate = const Duration(milliseconds: 800);
+    gameLoop(frameRate);
+  }
+
+  // game ㅣoop
+  void gameLoop(Duration frameRate) {
+    Timer.periodic(frameRate, (timer) {
+      setState(() {
+        // move current piece down
+        currentPiece.MovePiece(Direction.down);
+      });
+    });
+  }
+
+  // check for collision in a future position
+  // return true -> collision occured
+  bool checkCollision(Direction direction) {
+    // loop through each postion of the current piece
+    for (int i = 0; i < currentPiece.position.length; i++) {
+      // calculate the row and column of the current position
+      int row = (currentPiece.position[i] / rowLength).floor();
+      int column = (currentPiece.position[i] & rowLength);
+
+      // adjust the row and col based on direction
+      if (direction == Direction.left) {}
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +66,19 @@ class _GameBoardState extends State<GameBoard> {
             const NeverScrollableScrollPhysics(), // disable GridView' Scroll Function
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: rowLength),
-        itemBuilder: (context, index) => Pixel(
-          color: Colors.grey[900]!,
-          child: index.toString(),
-        ),
+        itemBuilder: (context, index) {
+          if (currentPiece.position.contains(index)) {
+            return Pixel(
+              color: Colors.yellow,
+              child: index.toString(),
+            );
+          } else {
+            return Pixel(
+              color: Colors.grey[900]!,
+              child: index.toString(),
+            );
+          }
+        },
       ),
     );
   }
